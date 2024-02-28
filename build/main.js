67d3c9b7a8318be7108b288280e4012acfc96b9e
kawamatsu0114 <kawamatsu0114@gmail.com>
2024-02-29 02:31:34 +0900

    test

--- build/main.js
+++ build/main.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,8 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 // import hey from "./commands/hey/hey";
 const dotenv_1 = __importDefault(require("dotenv"));
-const dispatcher_1 = __importDefault(require("./commands/dispatcher"));
-const dispatcher_2 = __importDefault(require("./reactions/dispatcher"));
 dotenv_1.default.config();
 const client = new discord_js_1.Client({
     intents: [
@@ -32,54 +21,56 @@ client.once(discord_js_1.Events.ClientReady, (c) => {
     console.log(`準備OKです！ ${c.user.tag}がログインします。`);
     createServer();
 });
-client.on(discord_js_1.Events.MessageCreate, (message) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b;
-    if (((_a = message.mentions.users.first()) === null || _a === void 0 ? void 0 : _a.id) !== process.env.APPLICATION_ID)
-        return;
-    if (message.content.split(" ")[0] !== `<@${process.env.APPLICATION_ID}>`)
-        return;
-    if (!((_b = message.member) === null || _b === void 0 ? void 0 : _b.permissions.has(discord_js_1.PermissionsBitField.Flags.Administrator))) {
-        message.reply("botの利用はサーバー管理者のみが可能となっています");
-        return;
-    }
-    console.log(message.content.split(" "));
-    try {
-        yield (0, dispatcher_1.default)(message);
-    }
-    catch (error) {
-        console.log(error);
-    }
-}));
-client.on(discord_js_1.Events.MessageReactionAdd, (reaction, user) => __awaiter(void 0, void 0, void 0, function* () {
-    var _c, _d, _e;
-    if (reaction.partial) {
-        try {
-            yield reaction.fetch();
-        }
-        catch (error) {
-            console.error("Something went wrong when fetching the message:", error);
-        }
-    }
-    if (!reaction.message.guild || !reaction.message.author) {
-        return;
-    }
-    const guild = yield reaction.message.guild.fetch();
-    if (((_c = guild.members.me) === null || _c === void 0 ? void 0 : _c.id) !==
-        ((_d = guild.members.cache.get(reaction.message.author.id)) === null || _d === void 0 ? void 0 : _d.id)) {
-        return;
-    }
-    if (!((_e = guild.members.cache
-        .get(user.id)) === null || _e === void 0 ? void 0 : _e.permissions.has(discord_js_1.PermissionsBitField.Flags.Administrator))) {
-        return;
-    }
-    if (!reaction.emoji.name) {
-        return;
-    }
-    const message = reaction.message.partial
-        ? yield reaction.message.fetch()
-        : reaction.message;
-    yield (0, dispatcher_2.default)(message, reaction.emoji.name);
-}));
+// client.on(Events.MessageCreate, async (message) => {
+//   if (message.mentions.users.first()?.id !== process.env.APPLICATION_ID) return;
+//   if (message.content.split(" ")[0] !== `<@${process.env.APPLICATION_ID}>`)
+//     return;
+//   if (
+//     !message.member?.permissions.has(PermissionsBitField.Flags.Administrator)
+//   ) {
+//     message.reply("botの利用はサーバー管理者のみが可能となっています");
+//     return;
+//   }
+//   console.log(message.content.split(" "));
+//   try {
+//     await commandDispatcher(message);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+// client.on(Events.MessageReactionAdd, async (reaction, user) => {
+//   if (reaction.partial) {
+//     try {
+//       await reaction.fetch();
+//     } catch (error) {
+//       console.error("Something went wrong when fetching the message:", error);
+//     }
+//   }
+//   if (!reaction.message.guild || !reaction.message.author) {
+//     return;
+//   }
+//   const guild = await reaction.message.guild.fetch();
+//   if (
+//     guild.members.me?.id !==
+//     guild.members.cache.get(reaction.message.author.id)?.id
+//   ) {
+//     return;
+//   }
+//   if (
+//     !guild.members.cache
+//       .get(user.id)
+//       ?.permissions.has(PermissionsBitField.Flags.Administrator)
+//   ) {
+//     return;
+//   }
+//   if (!reaction.emoji.name) {
+//     return;
+//   }
+//   const message = reaction.message.partial
+//     ? await reaction.message.fetch()
+//     : reaction.message;
+//   await reactionDispather(message, reaction.emoji.name);
+// });
 try {
     client.login(process.env.TOKEN);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
-import {
-  Client,
-  GatewayIntentBits,
-  Events,
-  PermissionsBitField,
-  Partials,
-} from "discord.js";
+import { Client, GatewayIntentBits, Events, Partials } from "discord.js";
 // import hey from "./commands/hey/hey";
 import dotenv from "dotenv";
-import commandDispatcher from "./commands/dispatcher";
-import reactionDispather from "./reactions/dispatcher";
 
 dotenv.config();
 
@@ -28,57 +20,57 @@ client.once(Events.ClientReady, (c: Client<true>) => {
   createServer();
 });
 
-client.on(Events.MessageCreate, async (message) => {
-  if (message.mentions.users.first()?.id !== process.env.APPLICATION_ID) return;
-  if (message.content.split(" ")[0] !== `<@${process.env.APPLICATION_ID}>`)
-    return;
-  if (
-    !message.member?.permissions.has(PermissionsBitField.Flags.Administrator)
-  ) {
-    message.reply("botの利用はサーバー管理者のみが可能となっています");
-    return;
-  }
-  console.log(message.content.split(" "));
-  try {
-    await commandDispatcher(message);
-  } catch (error) {
-    console.log(error);
-  }
-});
+// client.on(Events.MessageCreate, async (message) => {
+//   if (message.mentions.users.first()?.id !== process.env.APPLICATION_ID) return;
+//   if (message.content.split(" ")[0] !== `<@${process.env.APPLICATION_ID}>`)
+//     return;
+//   if (
+//     !message.member?.permissions.has(PermissionsBitField.Flags.Administrator)
+//   ) {
+//     message.reply("botの利用はサーバー管理者のみが可能となっています");
+//     return;
+//   }
+//   console.log(message.content.split(" "));
+//   try {
+//     await commandDispatcher(message);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
-client.on(Events.MessageReactionAdd, async (reaction, user) => {
-  if (reaction.partial) {
-    try {
-      await reaction.fetch();
-    } catch (error) {
-      console.error("Something went wrong when fetching the message:", error);
-    }
-  }
-  if (!reaction.message.guild || !reaction.message.author) {
-    return;
-  }
-  const guild = await reaction.message.guild.fetch();
-  if (
-    guild.members.me?.id !==
-    guild.members.cache.get(reaction.message.author.id)?.id
-  ) {
-    return;
-  }
-  if (
-    !guild.members.cache
-      .get(user.id)
-      ?.permissions.has(PermissionsBitField.Flags.Administrator)
-  ) {
-    return;
-  }
-  if (!reaction.emoji.name) {
-    return;
-  }
-  const message = reaction.message.partial
-    ? await reaction.message.fetch()
-    : reaction.message;
-  await reactionDispather(message, reaction.emoji.name);
-});
+// client.on(Events.MessageReactionAdd, async (reaction, user) => {
+//   if (reaction.partial) {
+//     try {
+//       await reaction.fetch();
+//     } catch (error) {
+//       console.error("Something went wrong when fetching the message:", error);
+//     }
+//   }
+//   if (!reaction.message.guild || !reaction.message.author) {
+//     return;
+//   }
+//   const guild = await reaction.message.guild.fetch();
+//   if (
+//     guild.members.me?.id !==
+//     guild.members.cache.get(reaction.message.author.id)?.id
+//   ) {
+//     return;
+//   }
+//   if (
+//     !guild.members.cache
+//       .get(user.id)
+//       ?.permissions.has(PermissionsBitField.Flags.Administrator)
+//   ) {
+//     return;
+//   }
+//   if (!reaction.emoji.name) {
+//     return;
+//   }
+//   const message = reaction.message.partial
+//     ? await reaction.message.fetch()
+//     : reaction.message;
+//   await reactionDispather(message, reaction.emoji.name);
+// });
 try {
   client.login(process.env.TOKEN);
 } catch (e) {
